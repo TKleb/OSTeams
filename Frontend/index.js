9ed265ca69bf774740e532dbb6750fcf15efb211
Dominic Walther <dominic.walther@ost.ch>
2022-03-10 10:18:43 +0100

    Add deployable docker file
    Requires "docker run" option "-p 3001:3001"

--- Frontend/index.js
+++ Frontend/index.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
     // load app with current config
     const app = (await import('./app.js')).app;
 
-    const hostname = '127.0.0.1';
+    const hostname = '0.0.0.0';
     const port = 3001;
     app.listen(port, hostname, () => {
         console.log(`Server running at http://${hostname}:${port}/`);
