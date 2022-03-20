c1dd878ae793133034e23d2b0865fa54d2b2a504
Ronny Müller <ronny.mueller@ost.ch>
2022-03-20 20:08:04 +0100

    prototype db access

--- Frontend/controller/module-controller.js
+++ Frontend/controller/module-controller.js
@@ -1,7 +1,29 @@
+import pgPromise from 'pg-promise';
 export class ModuleController {
     module(req, res) {
-        res.render("module", {title: "Module"});
+        const pgp = pgPromise({});
+
+        const cn = {
+            host: 'database',
+            port: 5432,
+            database: 'osteams',
+            user: 'backend',
+            password: 'password',
+            max: 30 // use up to 30 connections
+        };
+        const db = pgp(cn);
+        db.func('get_subjects')
+        .then(data => {
+            console.log('DATA:', data);
+            res.render("module", {title: data});
+        })
+        .catch(error => {
+            console.log('ERROR:', error); // print the error;
+            res.render("module", {title: error});
+        });
     };
 }
 
 export const moduleController = new ModuleController();
+
+
