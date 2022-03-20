4133831477dce75be7c81fbeb6ad46522732b329
Ronny Müller <ronny.mueller@ost.ch>
2022-03-20 21:18:17 +0100

    Prototype fix

--- Database/3_fill_with_sample_data.sql
+++ Database/3_fill_with_sample_data.sql
@@ -1,2 +1,2 @@
 INSERT INTO public."subjects" (subject_abbreviation,subject_name)
-VALUES ('DB1', 'Datenbanken 1');
+VALUES ('DB1', 'Datenbanken 1'), ('DB2', 'Datenbanken 2'), ('CN1', 'Computernetzwerke 1');
--- Frontend/controller/module-controller.js
+++ Frontend/controller/module-controller.js
@@ -1,7 +1,5 @@
 import pgPromise from 'pg-promise';
-export class ModuleController {
-    module(req, res) {
-        const pgp = pgPromise({});
+const pgp = pgPromise({});
 
         const cn = {
             host: 'database',
@@ -12,14 +10,18 @@ export class ModuleController {
             max: 30 // use up to 30 connections
         };
         const db = pgp(cn);
+
+export class ModuleController {
+    module(req, res) {
+        
         db.func('get_subjects')
         .then(data => {
             console.log('DATA:', data);
-            res.render("module", {title: data});
+            res.render("module", {modules: data});
         })
         .catch(error => {
             console.log('ERROR:', error); // print the error;
-            res.render("module", {title: error});
+            res.render("module", {modules: "Datbase Access didn't work: " + error});
         });
     };
 }
