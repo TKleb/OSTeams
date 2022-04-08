0c33e881177f0e17147304c4e18272adba64b571
Dominic Walther <dominic.walther@ost.ch>
2022-04-08 19:28:48 +0200

    Re-add uniqueness constraint on subject abbreviation

--- Database/1_create_tables.sql
+++ Database/1_create_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS subjects
 (
     subject_id INT GENERATED ALWAYS AS IDENTITY,
-    subject_abbreviation VARCHAR(10) NOT NULL,
+    subject_abbreviation VARCHAR(10) UNIQUE NOT NULL,
     subject_name VARCHAR(30) NOT NULL
 ) TABLESPACE pg_default;
 
