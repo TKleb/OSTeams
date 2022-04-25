8a4dca1b481bf2c21c0403ae411d072401bbb0ab
Dominic Walther <dominic.walther@ost.ch>
2022-04-25 18:30:53 +0200

    Improve subjects unit tests

--- Database/tests/9_fill_with_sample_data.sql
+++ Database/tests/9_fill_with_sample_data.sql
@@ -6,28 +6,21 @@
 
 DO
 $subject_tests$ BEGIN
-    IF NOT EXISTS(
-        SELECT * FROM add_subject('DB1', 'Datenbanken 1')
-    ) THEN
+    /* Fill with sample data */
+    INSERT INTO subjects(abbreviation, name) VALUES ('DB2', 'Datenbanken 2'), ('CN1', 'Computernetzwerke 1');
+
+    /* Tests */
+    IF 1 <> (SELECT COUNT(*) FROM add_subject('DB1', 'Datenbanken 1')) THEN
         RAISE EXCEPTION 'add_subject failed';
     END IF;
 
-    IF NOT EXISTS(
-        SELECT * FROM get_subjects()
-    ) THEN
+    IF (SELECT COUNT(*) FROM subjects) <> (SELECT COUNT(*) FROM get_subjects()) THEN
         RAISE EXCEPTION 'get_subjects failed';
     END IF;
 
-    IF NOT EXISTS(
-        SELECT * FROM get_subject_by_abbreviation('DB1')
-    ) THEN
+    IF 1 <> (SELECT COUNT(*) FROM get_subject_by_abbreviation('DB1')) THEN
         RAISE EXCEPTION 'get_subject_by_abbreviation failed';
     END IF;
-
-    /* Fill with sample data */
-    PERFORM add_subject('DB2', 'Datenbanken 2');
-    PERFORM add_subject('CN1', 'Computernetzwerke 1');
-
 END $subject_tests$;
 \echo subject_tests passed
 
