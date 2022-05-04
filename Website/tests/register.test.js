f6c092f47ea07a417e1a46ecb5e8422460dd70e3
Dejan Jovicic <dejan.jovicic@ost.ch>
2022-05-04 11:54:18 +0200

    Add account creation & verification test

--- Website/tests/register.test.js
+++ Website/tests/register.test.js
@@ -43,4 +43,14 @@ describe("Test register page", () => {
 			assert.equal(registerController.generateToken().length, 50);
 		});
 	});
+
+	describe("Create and verify new user", () => {
+		it("It should create and verify a new user.", async () => {
+			const token = registerController.generateToken();
+			registerController.addUnverifiedUserToDB((Math.random() + 1).toString(36).substring(20), registerController.hashPassword('123'), token);
+			const res = await chai.request(server)
+				.get(`/account/verifyEmail?token=${token}`);
+			assert.match(res.text, /Email verified successfully/);
+		});
+	});
 });
