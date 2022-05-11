0517834b20f9879bdbb73063bdaaf0240d904718
G <gianluca.nenz@ost.ch>
2022-05-11 14:23:34 +0200

    Add group creation & Refactor group controllers

--- Website/controller/grouplist-controller.js
+++
@@ -1,8 +0,0 @@
-class GroupListController {
-	index(req, res) {
-		const { abbreviation } = req.params;
-		res.render("grouplist", { title: "Groups", abbr: abbreviation });
-	}
-}
-
-export default new GroupListController();
--- Website/routes/subjects-routes.js
+++ Website/routes/subjects-routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import subjectsController from "../controller/subjects-controller.js";
-import groupListController from "../controller/grouplist-controller.js";
+import groupsController from "../controller/groups-controller.js";
 
 const router = express.Router();
 
 router.get("/", subjectsController.subjects);
 router.post("/", subjectsController.subjects);
 router.post("/add", subjectsController.insert);
-router.get("/:abbreviation", groupListController.index);
-
+router.get("/:abbreviation", groupsController.showBySubjectAbbr);
+router.post("/:abbreviation", groupsController.insert);
 export default router;
