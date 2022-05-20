d01f2f536eec53c7bd1be0af313aed042b630a5e
G <gianluca.nenz@ost.ch>
2022-05-20 20:48:09 +0200

    Add leave group functionality

--- Website/routes/groups-routes.js
+++ Website/routes/groups-routes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import groupsController from "../controller/groups-controller.js";
+import asyncHandler from "../middleware/asyncHandler.js";
 
 const router = express.Router();
 router.get("/", groupsController.showByUserId);
 router.get("/:id", groupsController.showGroupById);
+router.post("/leave/:id", asyncHandler(groupsController.leaveGroup));
+
 export default router;
