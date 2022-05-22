586ab1b77095441270f9ad81855113c078457add
Dominic Walther <dominic.walther@ost.ch>
2022-05-22 16:33:04 +0200

    Rework tests
    - Avoids use of magic numbers for IDs
    - Avoids tests altering database via rollbacks

--- Database/tests/9_fill_with_sample_data.sql
+++ Database/tests/9_fill_with_sample_data.sql
@@ -4,26 +4,67 @@
 -- Ref.: https://stackoverflow.com/questions/11299037/postgresql-if-statement --
 --------------------------------------------------------------------------------
 
+CREATE OR REPLACE FUNCTION before_each()
+    RETURNS VOID
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+AS $$
+    BEGIN
+        -- NOTE: Use direct inserts, not functions, as they are not proven to work at this point.
+
+        -- Add subjects
+        INSERT INTO subjects(abbreviation, name) VALUES ('TEST1', 'Use me in tests 1'), ('TEST2', 'Use me in tests 2');
+        -- Add users
+        INSERT INTO users(name, surname, password_hash, email) VALUES
+            ('In', 'Group', '123', 'in.group@ost.ch'),
+            ('NotIn', 'Group', '123', 'notin.group@ost.ch'),
+            ('John', 'Doe', '123', 'john.doe@ost.ch'),
+            ('Jane', 'Mow', '123', 'jane.mow@ost.ch');
+        -- Add unverified users
+        INSERT INTO unverified_users(name, surname, email, password_hash, verification_code) VALUES
+            ('Jaden', 'Doe', 'jaden.doe@ost.ch', '123', 'verifyme1'),
+            ('Jargon', 'Mow', 'jargon.mow@ost.ch', '123', 'verifyme2');
+        -- Add group
+        INSERT INTO groups(owner_id, name, subject_id, max_member_count,creation_date, apply_by_date) VALUES
+            (
+                (SELECT id FROM users WHERE email = 'in.group@ost.ch'),
+                'My first team',
+                (SELECT id FROM subjects ORDER BY id DESC LIMIT 1),
+                5,
+                NOW(),
+                NOW() + '3 days'::interval
+            );
+        INSERT INTO group_memberships(user_id, group_id, member_since) VALUES (
+            (SELECT id FROM users WHERE email = 'in.group@ost.ch'),
+            (SELECT id FROM groups ORDER BY id DESC LIMIT 1),
+            NOW()
+        );
+    END
+$$;
+
+--------------------------------------------------------------------------------
 DO
 $subject_tests$ BEGIN
     /* Fill with sample data */
-    INSERT INTO subjects(abbreviation, name) VALUES ('DB2', 'Datenbanken 2'), ('CN1', 'Computernetzwerke 1');
+    PERFORM before_each();
 
-    ASSERT 1 = COUNT(*) FROM add_subject('DB1', 'Datenbanken 1'),
+    ASSERT 1 = COUNT(*) FROM add_subject('NEWSUBJECT', 'Testing subjects'),
         'add_subject failed';
 
     ASSERT (SELECT COUNT(*) FROM subjects) = COUNT(*) FROM get_subjects(),
         'get_subjects failed';
 
-    ASSERT 1 = COUNT(*) FROM get_subject_by_abbreviation('DB1'),
+    ASSERT 1 = COUNT(*) FROM get_subject_by_abbreviation('NEWSUBJECT'),
         'get_subject_by_abbreviation failed';
-
+    ROLLBACK;
 END $subject_tests$;
 \echo subject_tests passed
 
 --------------------------------------------------------------------------------
 DO
 $user_tests$ BEGIN
+    PERFORM before_each();
+
     ASSERT 1 = COUNT(*) FROM add_unverified_user('Unverified2', 'User2', 'user1@verified.ch', '545', 'verCode1'),
         'add_unverified_user failed';
 
@@ -42,61 +83,93 @@ $user_tests$ BEGIN
     ASSERT 1 = COUNT(*) FROM get_user_by_email('user1@verified.ch'),
         'get_user_by_email failed';
 
+    ROLLBACK;
 END $user_tests$;
 \echo user_tests passed
 
 --------------------------------------------------------------------------------
 DO
 $group_tests$ BEGIN
-    ASSERT 1 = COUNT(*) FROM add_group('exampleGroup1', 1, 1, 'veryLongDescription', 5, '2012-12-21', '2069-4-20'),
-        'add_group failed';
+    -- Preparation
+    PERFORM before_each();
+    DELETE FROM groups;
 
-    ASSERT 1 = COUNT(*) FROM get_group_by_id(1),
+    -- Tests
+    ASSERT 1 = COUNT(*) FROM add_group(
+            'exampleGroup1',
+            (SELECT id FROM users WHERE email = 'notin.group@ost.ch'),
+            (SELECT id FROM subjects ORDER BY id DESC LIMIT 1),
+            'veryLongDescription',
+            5,
+            '2012-12-21',
+            '2069-4-20'
+        ), 'add_group failed';
+
+    ASSERT 1 = COUNT(*) FROM get_group_by_id((SELECT id FROM groups ORDER BY id DESC LIMIT 1)),
         'get_group_by_id failed';
 
-    ASSERT 1 = COUNT(*) FROM get_groups_by_subject_id(1),
+    ASSERT 1 = COUNT(*) FROM get_groups_by_subject_id((SELECT id FROM subjects ORDER BY id DESC LIMIT 1)),
         'get_groups_by_subject_id failed';
 
+    ROLLBACK;
 END $group_tests$;
 \echo group_tests passed
 
 --------------------------------------------------------------------------------
 DO
 $group_application_tests$ BEGIN
+    -- Preparation
+    PERFORM before_each();
 
-    PERFORM add_unverified_user('Unverified1', 'User2', 'user2@verified.ch', 'pwHash', 'verCode2');
-    PERFORM do_verify_user('verCode2');
+    -- Tests
+    ASSERT 1 = COUNT(*) FROM add_application(
+        (SELECT id FROM users WHERE email = 'notin.group@ost.ch'),
+        (SELECT id FROM groups ORDER BY id DESC LIMIT 1),
+        'Hi, i want to apply.',
+        '2012-12-21'
+    ), 'add_group failed';
 
-    ASSERT 1 = COUNT(*) FROM add_application(2, 1, 'Hi, i want to apply.', '2012-12-21'),
-        'add_group failed';
+    ASSERT 1 = COUNT(*) FROM get_applications_to_group(
+        (SELECT id FROM groups ORDER BY id DESC LIMIT 1)
+    ), 'get_applications_to_group failed';
 
-    ASSERT 1 = COUNT(*) FROM get_applications_to_group(1),
-        'get_applications_to_group failed';
+    ASSERT 0 = COUNT(*) FROM do_close_application(
+        (SELECT id FROM group_applications ORDER BY id DESC LIMIT 1),
+        FALSE
+    ), 'do_close_application failed';
 
-    ASSERT 0 = COUNT(*) FROM do_close_application(1, FALSE),
-        'do_close_application failed';
+    ASSERT 0 = COUNT(*) FROM get_applications_to_group(
+        (SELECT id FROM groups ORDER BY id DESC LIMIT 1)
+    ), 'get_applications_to_group failed';
 
-    ASSERT 0 = COUNT(*) FROM get_applications_to_group(1),
-        'get_applications_to_group failed';
+    ROLLBACK;
 END $group_application_tests$;
 \echo group_application_tests passed
 
 --------------------------------------------------------------------------------
 DO
 $group_memberships_tests$ BEGIN
-    /* Fill with sample data */
-    ASSERT 1 = COUNT(*) FROM get_groups_of_user_by_id(1),
-        'get_groups_by_user_id failed';
+    -- Preparation
+    PERFORM before_each();
+    INSERT INTO group_memberships(user_id, group_id, member_since) VALUES (
+        (SELECT id FROM users WHERE email = 'notin.group@ost.ch'),
+        (SELECT id FROM groups ORDER BY id DESC LIMIT 1),
+        NOW()
+    );
 
-    ASSERT 1 = COUNT(*) FROM get_members_by_group_id(1),
-        'get_members_by_group_id failed';
+    -- Tests
+    ASSERT 1 = COUNT(*) FROM get_groups_of_user_by_id(
+        (SELECT id FROM users WHERE email = 'notin.group@ost.ch')
+    ), 'get_groups_of_user_by_id failed wint regular member';
 
-    /*PERFORM do_remove_user_from_group(1, 1);*/
+    ASSERT 1 = COUNT(*) FROM get_groups_of_user_by_id(
+        (SELECT id FROM users WHERE email = 'in.group@ost.ch')
+    ), 'get_groups_of_user_by_id failed with owner';
 
-    PERFORM add_unverified_user('Peter', 'Lustig', 'peter@lustig.ch', 'pwHash', 'verCode3');
-    PERFORM do_verify_user('verCode3');
-    PERFORM add_application(3, 1, 'Hi, i want to apply.', '2012-12-21');
-    PERFORM do_close_application(2, TRUE);
+    ASSERT 2 = COUNT(*) FROM get_members_by_group_id(
+        (SELECT id FROM groups ORDER BY id DESC LIMIT 1)
+    ), 'get_members_by_group_id failed';
 
+    ROLLBACK;
 END $group_memberships_tests$;
 \echo group_memberships_tests passed
