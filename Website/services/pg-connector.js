23e0bfe630456061fa215a419f21849ff9ac9343
Dominic Walther <dominic.walther@ost.ch>
2022-05-25 16:25:22 +0200

    Add getUserById() to pg-connector

--- Website/services/pg-connector.js
+++ Website/services/pg-connector.js
@@ -1,6 +1,14 @@
 import pgPromise from "pg-promise";
 import pgConfig from "../config/postgres.config.js";
 
+function getFirst(reply) {
+	if (!reply || reply.length < 1) {
+		return undefined;
+	} else {
+		return reply[0];
+	}
+}
+
 class PGConnector {
 	constructor() {
 		const pgp = pgPromise({});
@@ -13,6 +21,11 @@ class PGConnector {
 			.catch((err) => {
 				throw new Error("Couldn't invoke Stored Procedure: ".concat(err));
 			});
+	}
+
+	getUserById(id) {
+		return this.executeStoredProcedure("get_user_by_id", [id])
+			.then(getFirst);
 	}
 }
 
