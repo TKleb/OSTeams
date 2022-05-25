4acdb115d407596c5bed8e770ee2ab96a5faadcc
Ronny Müller <ronny.mueller@ost.ch>
2022-05-25 13:37:02 +0200

    test added

--- Database/tests/7_run_tests.sql
+++ Database/tests/7_run_tests.sql
@@ -77,6 +77,9 @@ $user_tests$ BEGIN
     ASSERT 1 = COUNT(*) FROM get_user_by_email('user1@verified.ch'),
         'get_user_by_email failed';
 
+    ASSERT 1 = COUNT(*) FROM get_user_by_id(1),
+        'get_user_by_id failed';
+
     ROLLBACK;
 END $user_tests$;
 \echo user_tests passed
