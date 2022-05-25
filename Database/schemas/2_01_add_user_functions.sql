c99b341dc5020a97e94bd62d5b5a908cdbbb02d1
Ronny Müller <ronny.mueller@ost.ch>
2022-05-25 13:35:12 +0200

    get_user_by_id

--- Database/schemas/2_01_add_user_functions.sql
+++ Database/schemas/2_01_add_user_functions.sql
@@ -211,7 +211,7 @@ GRANT ALL ON FUNCTION do_remove_user_from_group TO backend;
 CREATE OR REPLACE FUNCTION get_groups_of_user_by_id(
     p_user_id INT
 )
-    RETURNS SETOF groups
+RETURNS SETOF groups
 LANGUAGE plpgsql
 SECURITY DEFINER
 AS $$
@@ -224,3 +224,19 @@ AS $$
 $$;
 
 GRANT ALL ON FUNCTION get_groups_of_user_by_id TO backend;
+
+-- get_user_by_id
+CREATE OR REPLACE FUNCTION get_user_by_id(
+    p_user_id INT
+)
+RETURNS SETOF users
+LANGUAGE plpgsql
+SECURITY DEFINER
+AS $$
+    BEGIN
+        RETURN QUERY
+        SELECT * FROM users WHERE id = p_user_id;
+    END
+$$;
+
+GRANT ALL ON FUNCTION get_user_by_id TO backend;
