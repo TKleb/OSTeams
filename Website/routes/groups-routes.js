608f8b57f2f03c3ae4c052d442da036e3864c904
Dominic Walther <dominic.walther@ost.ch>
2022-05-26 23:24:57 +0200

    Prevent hard crash on invalid URL

--- Website/routes/groups-routes.js
+++ Website/routes/groups-routes.js
@@ -3,8 +3,8 @@ import groupsController from "../controller/groups-controller.js";
 import asyncHandler from "../middleware/asyncHandler.js";
 
 const router = express.Router();
-router.get("/", groupsController.showByUserId);
-router.get("/:id", groupsController.showGroupById);
+router.get("/", asyncHandler(groupsController.showByUserId));
+router.get("/:id", asyncHandler(groupsController.showGroupById));
 router.post("/leave/:id", asyncHandler(groupsController.leaveGroup));
 router.post("/delete/:id", asyncHandler(groupsController.deleteGroup));
 router.post("/applyTo/:id", asyncHandler(groupsController.applyToGroup));
