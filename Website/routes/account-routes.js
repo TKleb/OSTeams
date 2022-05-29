f8dd7bfd8229a1cb1513a4968b11e9813d418ab0
G <gianluca.nenz@ost.ch>
2022-05-29 13:59:32 +0200

    Remove forgot password

--- Website/routes/account-routes.js
+++ Website/routes/account-routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import accountController from "../controller/account-controller.js";
 import registerController from "../controller/register-controller.js";
 import loginController from "../controller/login-controller.js";
-import forgotPwController from "../controller/forgotPassword-controller.js";
 import { isAuthenticated, isUnauthenticated } from "../middleware/auth.js";
 import asyncHandler from "../middleware/asyncHandler.js";
 
@@ -11,12 +10,10 @@ router.get("/", isAuthenticated, accountController.index);
 router.get("/edit", isAuthenticated, accountController.edit);
 router.get("/logout", isAuthenticated, accountController.logout);
 router.get("/login", isUnauthenticated, loginController.index);
-router.get("/forgot", isUnauthenticated, forgotPwController.index);
 router.get("/register", isUnauthenticated, registerController.index);
 router.get("/verifyEmail", isUnauthenticated, registerController.verifyMail);
 
 router.post("/login", isUnauthenticated, asyncHandler(loginController.login));
-router.post("/forgot", isUnauthenticated, asyncHandler(forgotPwController.forgotPassword));
 router.post("/register", isUnauthenticated, asyncHandler(registerController.register));
 
 export default router;
