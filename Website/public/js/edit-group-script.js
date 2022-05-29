1f2dd7b287e009daf6aaacf522a6cd033367dab2
G <gianluca.nenz@ost.ch>
2022-05-29 12:29:08 +0200

    Add client side apply by validation

---
+++ Website/public/js/edit-group-script.js
@@ -0,0 +1,4 @@
+import { setMinMaxApplyByDate } from "./apply-by-date-script.js";
+
+const editGroupApplyByDate = document.getElementById("edit-group-applydate");
+setMinMaxApplyByDate(editGroupApplyByDate);
