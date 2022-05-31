099fdc6897454897ff704005d271292577e84fa6
Dominic Walther <dominic.walther@ost.ch>
2022-05-31 14:20:13 +0200

    Rebuild db on test.bat

--- Website/test.bat
+++ Website/test.bat
@@ -5,4 +5,6 @@ setlocal
 set COMPOSE_DOCKER_CLI_BUILD=1
 set DOCKER_BUILDKIT=1
 set TARGET=test
+docker-compose rm -sf db
+call powershell -command "docker volume rm $(docker volume ls -f name=.*postgres-db -q) --force"
 cd .. && docker-compose up --build web
