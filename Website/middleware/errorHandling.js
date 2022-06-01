8139fc14627b9075ac2f5a07c5dbf16e9b220727
Dominic Walther <dominic.walther@ost.ch>
2022-06-01 09:19:46 +0200

    Fix linter

--- Website/middleware/errorHandling.js
+++ Website/middleware/errorHandling.js
@@ -1,5 +1,4 @@
-import websiteConfig from "../config/website.config.js";
-
+// eslint-disable-next-line no-unused-vars
 export default (err, req, res, next) => {
 	console.error(err.message);
 	req.flash("error", "An internal error has occurred.");
